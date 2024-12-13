404f7761cfac6618de70c0041bd37e9370969815
vastayaa <29062004@inbox.ru>
2024-12-13 14:54:09 +0300

    microchanges

--- api_modules/microservices/api.initial.functions.mjs
+++ api_modules/microservices/api.initial.functions.mjs
@@ -9,15 +9,15 @@ import { handleStartMessage, handleError } from "./api.deborah.mjs"
 // Module =======================================================================================================================================================================================================================>
 export const initialSystem = async () => {
   try {
-    const token = jwt.sign({ clientType: 'backend', prjName: 'Dora', destinationIp: '127.0.0.10', destinationPort: 5000 }, "1dmscnj823?/dsad_02sdawq-ds", { algorithm: 'HS512', expiresIn: '1m' });
+    const token = jwt.sign({ prjName: 'Dora', destinationIp: '127.0.0.10', destinationPort: 5000 }, "1dmscnj823?/dsad_02sdawq-ds", { algorithm: 'HS512', expiresIn: '1m' });
     const status = await redis.get('Dora:State')
-    const buildVersion = '0D19M11Y24B'; // Version/day/month/year/stable
+    const buildVersion = '0D06M12Y24B'; // Version/day/month/year/stable
     const startLoadTimestamp = new Date();
     const version = '0.0.1';
 
     try {
       redis.set('Dora:State', 'BFU');
-      const resp = await axios.post('http://127.0.0.100:4430', { "request": "backend" }, { 'x-proxy-token': token });
+      const resp = await axios.post('https://127.0.0.100:4430', { "request": "backend" }, { headers: { 'x-proxy-token': token } });
       console.log(resp?.data);
       await redis.set('Dora:State', 'AFU');
     } catch (e) { console.log(`Cocoa First Initial Error, ${e}`) }
